ed16900a0720ee87fabcd436984d14e3f0fcda54
Arch0wl <annstaroviotov@gmail.com>
2022-05-23 09:31:21 -0400

    magic changes

---
+++ src/components/RestaurantList.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { Col, Row, Card } from 'antd';
+
+function RestaurantList() {
+    const [restaurants, setRestaurants] = useState();
+    useEffect(() => {
+         // fetch our API 
+         fetch('http://my-first-firestore-hs.web.app/restaurants')
+         .then (response => response.json())
+         .then (data => setRestaurants(data))
+         .catch(console.error)
+    }, [])
+  
+    return (
+        <section>
+            <Row gutter={16}>
+            {!restaurants
+                ? <h2>Loading...</h2>
+                : restaurants.map(restaurant => (
+                    <Col style={{width:'300px', margin: '1em'}} key={restaurant.id}>
+                        <Card
+                        cover={<img alt={`Typical meal at ${(restaurant.name)}`} src={restaurant.image} />}
+                        hoverable>
+                            <Card.Meta title={restaurant.name} description={restaurant.address} />
+                        {restaurant.name}
+                    <p>{restaurant.address}</p>
+                    </Card>
+                    </Col>
+                )
+            )}
+            </Row>
+        </section>
+    )
+}
+
+export default RestaurantList;
