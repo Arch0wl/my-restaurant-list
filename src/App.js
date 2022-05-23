ca2fdb313b09ac3dab1c247a7662265c0da5a7a3
Arch0wl <annstaroviotov@gmail.com>
2022-05-23 11:29:49 -0400

    changes to onClick button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Layout } from 'antd';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Menubar from './components/Menubar';
 import RestaurantList from './components/RestaurantList';
 import './App.css';
@@ -7,14 +8,22 @@ const { Header, Content } = Layout;
 
 function App() {
   return (
+    <BrowserRouter>
     <Layout className='layout'>
       <Header>
         <Menubar />
       </Header>
       <Content>
+        <Routes>
+          <Route path='/random' element={<h1>Random</h1>} />
+          <Route path='/add' element={<h1>Add Restaurant</h1>} />
+          <Route path='/login' element={<h1>Login</h1>} />
+          <Route path='/' element={<RestaurantList />} />
+        </Routes>
         <RestaurantList />
       </Content>
     </Layout>
+    </BrowserRouter>
   );
 }
 
