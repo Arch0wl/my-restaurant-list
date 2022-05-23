9becfe598e28c2cce522187740d4d6d3985828d8
Arch0wl <annstaroviotov@gmail.com>
2022-05-23 19:11:20 -0400

    adding email

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import { Layout } from 'antd';
+import { useState, createContext } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 import Menubar from './components/Menubar';
 import RestaurantList from './components/RestaurantList';
+import RestaurantPage from './components/RestaurantPage';
+import Login from './components/Login';
 import './App.css';
 
 const { Header, Content } = Layout;
 
+export const UserContext = createContext(null); 
+
 function App() {
+  const [user, setUser] = useState();
   return (
     <BrowserRouter>
-    <Layout className='layout'>
-      <Header>
-        <Menubar />
-      </Header>
-      <Content>
-        <Routes>
-          <Route path='/random' element={<h1>Random</h1>} />
-          <Route path='/add' element={<h1>Add Restaurant</h1>} />
-          <Route path='/login' element={<h1>Login</h1>} />
-          <Route path='/' element={<RestaurantList />} />
-        </Routes>
-        <RestaurantList />
-      </Content>
-    </Layout>
+    <UserContext.Provider value={{ user, setUser }}>
+      <Layout className='layout'>
+        <Header>
+          <Menubar />
+        </Header>
+        <Content>
+          <Routes>
+            <Route path='/restaurants/:restaurantId' element={<RestaurantPage />} />
+            <Route path='/random' element={<h1>Random</h1>} />
+            <Route path='/add' element={
+              !user 
+              ? <Login /> 
+              : <h1>Add Restaurant</h1>
+              } />
+            <Route path='/' element={<RestaurantList />} />
+          </Routes>
+        </Content>
+      </Layout>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Col, Card } from 'antd';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Col, Card, Rate } from 'antd';
+import { UserContext } from '../App';
 const { Meta } = Card;
 
+
 export default function RestaurantCard({ restaurant }) {
+    let navigate = useNavigate();
+    const { user } = useContext(UserContext);
   return (
     <Col style={{ width: '300px', margin: '1em' }}>
       <Card
-        cover={<img alt={`Typical meal at ${restaurant.name}`} src={restaurant.image} />}
+      onClick={() => navigate(`/restaurants/${restaurant?.id}`)}
+        cover={restaurant && <img alt={`Typical meal at ${restaurant.name}`} src={restaurant?.image} />}
         hoverable>
-        <Meta title={restaurant.name} description={restaurant.address} />
+        <Meta title={restaurant?.name} 
+        description={restaurant?.address} />
+        {user && <Rate />}
       </Card>
     </Col>
   )
